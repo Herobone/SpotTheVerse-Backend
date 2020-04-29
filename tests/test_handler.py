d7c4a8b87062e91a89b3ba29dc04c0c1628e6968
Herobone <theherobone@gmail.com>
2020-04-29 10:56:09 +0200

    Changed test, dont't need it but nice2have

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -1,16 +1,16 @@
 import unittest
-import index
+from src import index
 
 
 class TestHandlerCase(unittest.TestCase):
 
     def test_response(self):
         print("testing response.")
-        result = index.handler(None, None)
+        result = index.handler()
         print(result)
-        self.assertEqual(result['statusCode'], 200)
-        self.assertEqual(result['headers']['Content-Type'], 'application/json')
-        self.assertIn('Hello World', result['body'])
+        self.assertEqual(result[1], 200)
+        self.assertEqual(result[2]['Content-Type'], 'application/json')
+        self.assertIn('Hello World 2', result[0])
 
 
 if __name__ == '__main__':
