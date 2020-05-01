211a8a0821a5c071e649b6dd8e5c848efef2af30
Herobone <theherobone@gmail.com>
2020-05-01 20:23:47 +0200

    Can process /analyse

--- src/LambdaProcessor.py
+++ src/LambdaProcessor.py
@@ -1,15 +1,30 @@
+import Util
+import json
+
+class Request:
+    def __init__(self, event):
+        self.headers = event["headers"]
+        self.method = event["httpMethod"]
+        self.isBase64 = event["isBase64Encoded"]
+        self.getParams = event["queryStringParameters"]
+        self.path = event["path"]
+        self.pathParams = event["pathParameters"]
+        self.json = json.loads(event["body"])
+
 class Processor:
     def __init__(self):
         self.REGS = dict()
-        pass
 
     def __call__(self, event, context):
+        request = Request(event)
+        
         if event["resource"] in self.REGS:
             func = self.REGS[event["resource"]]
             if event["pathParameters"] != None:
-                return func(**event["pathParameters"])
+                return func(request, **event["pathParameters"])
             else:
-                return func()
+                return func(request)
+    
 
     def register(self, additional):
 
--- src/othertest.py
+++ src/othertest.py
@@ -1,21 +1,45 @@
 from LambdaProcessor import Processor
 import json
+from Playlist import PlaylistAPI
+import Util
 
 proc = Processor()
 
 @proc.register("/analyse/{id}")
-def test1(id):
-    print("Test 1")
-    print(f"ID is {id}")
-    return standard()
-
 @proc.register("/analyse")
-def test2():
-    print("Test 2")
-    return standard()
+def test2(request: 'Request', id=None):
+
+    token = request.headers.get("Token")
+
+    if token == None:
+        return json_response({"error": "No token provided"}, response_code=401)
+
+    data = {"token": token}
+
+    if id == None:
+        # From Databse?
+        data = {"message": "from db"}
+        id = "37i9dQZF1DZ06evO2dYtbO"
+
+    
+    playlist = PlaylistAPI(id, token)
+
+    if request.method == "GET":
+        # Analyse and get Songs
+        playlist.getFeatures()
+        data = {"message": "get features"}
+        data = playlist.analyse(Util.better_mean, roundStage=PlaylistAPI.ROUND_POST, roundAccuracy=3).toJSON()
+        pass
+    elif request.method == "POST":
+        # Playlist data should be given
+        dictin = request.get_json()
+        test = dictin["items"]
+        data = {"message": f"from post with data {test}"}
+
+    return json_response(data)
 
 @proc.register("/")
-def test3():
+def test3(request):
     print("Test 3")
     return standard()
 
